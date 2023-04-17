250d6a96c464f17e4318965a1a40074f92416c29
Alexandra <alexandra@MacBook-Air-Alexandra.local>
2023-04-18 01:55:28 +0400

    init: add task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,11 +1,13 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+
 class BinarySearchTree {
   constructor() {
     this._root = null;
@@ -15,54 +17,102 @@ class BinarySearchTree {
     return this._root;
   }
 
-  add(data) {
-    const node = new Node(data);
+  add(value) {
+    const node = new Node(value);
     if (!this._root) {
       this._root = node;
-    } else {
-      let current = this._root;
-
-      while (true) {
-        if (data < current.data) {
-          if (!current.left) {
-            current.left = node;
-            break;
-          }
-          current = current.left;
-        } else {
-          if (!current.right) {
-            current.right = node;
-            break;
-          }
-          current = current.right;
+      return;
+    }
+    let current = this._root;
+    while (current) {
+      if (node.data < current.data) {
+        if (!current.left) {
+          current.left = node;
+          break;
         }
+        current = current.left;
+      } else {
+        if (!current.right) {
+          current.right = node;
+          break;
+        }
+        current = current.right;
       }
     }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(value) {
+    let current = this._root;
+    while (current) {
+      if (value === current.data) {
+        return current;
+      }
+      current = value < current.data ? current.left : current.right;
+    }
+    return null;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(value) {
+    return !!this.find(value);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(value) {
+    let current = this._root;
+    let parent = null;
+    while (current) {
+      if (value === current.data) {
+        if (!current.left && !current.right) {
+          if (!parent) {
+            this._root = null;
+          } else if (current === parent.left) {
+            parent.left = null;
+          } else {
+            parent.right = null;
+          }
+        } else if (current.left && !current.right) {
+          if (!parent) {
+            this._root = current.left;
+          } else if (current === parent.left) {
+            parent.left = current.left;
+          } else {
+            parent.right = current.left;
+          }
+        } else if (!current.left && current.right) {
+          if (!parent) {
+            this._root = current.right;
+          } else if (current === parent.left) {
+            parent.left = current.right;
+          } else {
+            parent.right = current.right;
+          }
+        } else {
+          let temp = current.right;
+          while (temp.left) {
+            temp = temp.left;
+          }
+        }
+        return true;
+      }
+      parent = current;
+      current = value < current.data ? current.left : current.right;
+    }
+    return false;
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let current = this._root;
+    while (current && current.left) {
+      current = current.left;
+    }
+    return current ? current.data : null;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let current = this._root;
+    while (current && current.right) {
+      current = current.right;
+    }
+    return current ? current.data : null;
   }
 }
 
